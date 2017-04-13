fbdc1b3d4c5e43648d20893e0a2aa00a0e982612
jaivardhan <mailjai.vardhan@gmail.com>
2017-04-13 11:35:50 +0530

    socket send and receive message

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+thread = None
 
 
 @socketio.on('echo')
 def handle_my_custom_namespace_event(jsn):
     print('received json: ' + str(jsn))
     return {'msg': jsn}
+
+@socketio.on('my_ping', namespace='/test')
+def ping_pong():
+    emit('my_pong')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
 
 if __name__ == "__main__":
     socketio.run(app, port=5012)
